477aba47a17ec6ba28efe5aff4b3c3e7bd880713
S E P E H R _ H A S H E M I <sepehrhashemimamaquani1996@gmail.com>
2023-09-11 19:48:06 +0330

    modify-files

---
+++ src/app/products/page.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Product from "@/components/UI/product/Product";
+
+type Product = {
+    product: string
+    price: string
+    id: string
+}
+const getProducts = async () => {
+    const response = await fetch("https://64db99de593f57e435b13182.mockapi.io/products")
+    const data = await response.json()
+    return data;
+}
+const Products = async () => {
+    const product: Product[] = await getProducts();
+    return (
+        <div className={"flex flex-col space-y-3 px-10"}>
+            <h2>Products Page</h2>
+            {product.length > 0 ? product.map(product => {
+                return <Product title={product.product} price={product.price} key={product.id} id={product.id}/>
+            }) : null }
+        </div>
+    );
+};
+
+export default Products;
